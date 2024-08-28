18b59ffb92e7d91056557ef3cbb6fa98b0b95884
David Cuadros <cuadrosdavid01@gmail.com>
2024-08-28 05:05:45 +0000

    f

--- server/server.py
+++ server/server.py
@@ -11,12 +11,39 @@ class ServicioSistema(SistemaServicer):
         print("Solicitud de operación recibida.")
 
 def start():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    server1 = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     add_SistemaServicer_to_server(ServicioSistema(), server)
-    server.add_insecure_port('[::]:5000')
-    print("El servidor está ejecutándose. Esperando solicitud de cliente.")
-    server.start()
-    server.wait_for_termination()
+    server1.add_insecure_port('[::]:5000')
+    print("El servidor principal se está ejecutando. Esperando solicitud de cliente.")
+    server1.start()
+    
+    server1.wait_for_termination()
+
+    # -------------------------------------------
+
+def enviar_solicitud(servidor, tipo_operacion, dato):
+    with grpc.insecure_channel(servidor) as channel:
+        stub = SistemaStub(channel)
+        solicitud = OperacionRequest(tipoOperacion=tipo_operacion, dato=dato)
+        respuesta = stub.OperacionParcial(solicitud)
+        return respuesta.resultado
 
 if __name__ == "__main__":
     start()
+
+    servidores = [
+        'localhost:5001',
+        'localhost:5002',
+        'localhost:5003',
+        'localhost:5004'
+    ]
+    dato = 10
+    resultados = []
+
+    for i, servidor in enumerate(servidores):
+        tipo_operacion = i + 1  # El tipo de operación varía entre 1 y 4
+        resultado = enviar_solicitud(servidor, tipo_operacion, dato)
+        print(f"Resultado de la operación en el servidor {servidor}: {resultado}")
+        resultados.append(resultado)
+
+    print(f"Resultado Total {sum(resultados)}")
---
+++ servidoresOperaciones/servidor2.py
@@ -0,0 +1,38 @@
+import grpc
+from concurrent import futures
+from servicio_pb2_grpc import SistemaServicer, add_SistemaServicer_to_server
+from servicio_pb2 import ResultadoReceive, OperacionRequest
+
+class ServicioSistema(SistemaServicer):
+    def Listo(self, request, context):
+        return request
+
+    def OperacionParcial(self, request, context):
+        tipo_operacion = request.tipoOperacion
+        dato = request.dato
+        resultado = 0
+
+        if tipo_operacion == 1:
+            resultado = dato * 2
+        elif tipo_operacion == 2:
+            resultado = dato + 2
+        elif tipo_operacion == 3:
+            resultado = dato / 2
+        elif tipo_operacion == 4:
+            resultado = dato ** 2
+        else:
+            context.set_details('Operación no válida')
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+
+        return ResultadoReceive(resultado=resultado)
+
+def start():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    add_SistemaServicer_to_server(ServicioSistema(), server)
+    server.add_insecure_port('[::]:5002')
+    print("El servidor 2 está ejecutándose. Esperando solicitud de cliente.")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    start()
